b21ae92ffccfaac7b039fea99b3ff62f56cf59f4
Denis <pozitiven4el@mail.ru>
2025-03-03 20:29:28 +0300

    Refined searching by post tag

--- blog/views.py
+++ blog/views.py
@@ -4,18 +4,23 @@ from .models import *
 from django.shortcuts import get_object_or_404
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models import F
+from taggit.models import Tag
+
 
 
 def post_list(request, tag_slug=None):
     posts = Post.objects.all()
+    tag_name = None
     if tag_slug:
-        posts = Post.objects.filter(tags__slug__in=[tag_slug])
+        tag = Tag.objects.filter(slug=tag_slug).first()
+        if tag:
+            tag_name = tag.name
+            posts = Post.objects.filter(tags__slug__in=[tag_slug])
     p = Paginator(posts, 3)
     page = request.GET.get("page", 1)
     page_obj = p.page(page)
     page_obj.elided_page_range = p.get_elided_page_range(number=page)
-    return render(request, "blog/index.html", {"posts": posts, "page_obj": page_obj, "tag_slug": tag_slug})
-
+    return render(request, "blog/index.html", {"posts": posts, "page_obj": page_obj, "tag_name": tag_name})
 
 
 def post_detail(request, post_id):
@@ -32,3 +37,4 @@ def post_search(request):
             combined_similarity = F("title_similarity") + F("body_similarity")
         ).filter(combined_similarity__gt=0.1)
         return render(request, "blog/search.html", {"posts": posts})
+
